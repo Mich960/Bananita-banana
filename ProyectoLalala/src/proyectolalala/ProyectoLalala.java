a5be15a16aad06c6e952db5a2ee4f4515a43bc72
Mich <Mich@192.168.228.136>
2016-03-31 17:17:16 -0600

    Pruebas de git

---
+++ ProyectoLalala/src/proyectolalala/ProyectoLalala.java
@@ -0,0 +1,20 @@
+/*
+ 
+        comento comentario :D
+ */
+package proyectolalala;
+
+/**
+ *
+ * @author Mich
+ */
+public class ProyectoLalala {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
