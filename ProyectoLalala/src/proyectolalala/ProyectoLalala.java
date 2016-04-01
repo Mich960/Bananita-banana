a75beeab7192b479ba8c95b3fed3aa344c1e52fe
Mich <Mich@192.168.228.136>
2016-03-31 18:03:26 -0600

    ...

--- ProyectoLalala/src/proyectolalala/ProyectoLalala.java
+++ ProyectoLalala/src/proyectolalala/ProyectoLalala.java
@@ -7,21 +7,26 @@ package proyectolalala;
 import java.lang.*;
 
 /**
- *
  * @author Mich
  */
 public class ProyectoLalala {
 
+    public class nohayinternet{
+        int internet=0;
+    }
+    
     
     public class Hambre{
         char v33= 'l', v1='e', v2='l';
         
     }
     
-    
+    //y los perros ladran
     
     public static void main(String[] args) {
         // TODO code application logic here
     }
     
 }
+
+//y los gatos maullan
